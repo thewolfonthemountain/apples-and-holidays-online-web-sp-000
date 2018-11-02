4b166086978888fa6631a3aba6e5833819d56ac7
Sean Langton <slangton1@gmail.com>
2018-11-02 02:07:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season,holiday|
   puts "#{season[0].upcase + season[1..-1]}:"
   holiday.each do |attribute,data|
-      puts "  #{attribute[0].upcase + attribute[1..-1]}: #{data}"
+      puts "  #{attribute[0].upcase + attribute[1..-1]}: #{data.values.flatten}"
   end
 end
 end
